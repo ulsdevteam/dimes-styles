bf00d32e0f769f11a95202d63a68c20c92125ad6
Hillel Arnold <helrond@hotmail.com>
2021-04-16 16:06:41 -0400

    add presets

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,13 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-a11y"
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-docs',
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
+    '@storybook/preset-scss'
   ]
 }
