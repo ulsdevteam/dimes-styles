f3c686eb2a3b5d81004cfb4c7db332f3ac08233a
Hillel Arnold <helrond@hotmail.com>
2021-04-19 11:52:39 -0400

    convert button to handlebars template

--- stories/button/Button.stories.js
+++ stories/button/Button.stories.js
@@ -2,10 +2,10 @@ import { storiesOf } from '@storybook/html'
 
 import '../../dist/main.min.css'
 
-import Primary from './primary.html'
-import IconAfter from './iconAfter.html'
-import IconBefore from './iconBefore.html'
+import Button from './button.handlebars'
+import buttonData from './button.json'
+
 storiesOf('Buttons', module)
-  .add('Primary button', () => Primary, {})
-  .add('Leading icon', () => IconBefore, {})
-  .add('Trailing icon', () => IconAfter, {})
+  .add('Button', () => {
+    return Button(buttonData)
+  })
