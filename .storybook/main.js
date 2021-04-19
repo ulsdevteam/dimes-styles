98bc4ef920a78acfe57a2aa12d1c19d5e57026af
Hillel Arnold <helrond@hotmail.com>
2021-04-19 11:52:22 -0400

    add handlebars loaders

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,5 +9,19 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/addon-a11y',
     '@storybook/preset-scss'
-  ]
+  ],
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    config.module.rules.push({
+      test: /\.handlebars$/,
+      use: ['handlebars-loader'],
+    });
+
+    // Return the altered config
+    return config;
+  }
 }
