fc09be1e06b43b215664831a743b9f2645adb878
Hillel Arnold <helrond@hotmail.com>
2021-04-19 15:01:49 -0400

    get button story into a good place

--- stories/button/Button.stories.js
+++ stories/button/Button.stories.js
@@ -4,8 +4,31 @@ import '../../dist/main.min.css'
 
 import Button from './button.handlebars'
 import buttonData from './button.json'
+import colors from './colors.json'
+import sizes from './sizes.json'
 
-storiesOf('Buttons', module)
-  .add('Button', () => {
-    return Button(buttonData)
-  })
+export default {
+  title: 'Components/Button',
+  component: Button,
+  args: {
+    text: 'Click me'
+  }
+}
+
+export const Colors = (args) => {
+  const buttons = colors.map(c => Button({ ...buttonData, class: c, ...args })).join('')
+  return buttons
+}
+
+export const Sizes = (args) => {
+  const buttons = sizes.map(s => Button({ ...buttonData, class: s, ...args })).join('')
+  return buttons
+}
+
+export const Icons = (args) => Button({ ...buttonData, iconAfter: 'email', ...args })
+Icons.args = {
+  iconBefore: 'email',
+  iconAfter: ''
+}
+
+export const FullWidth = (args) => Button({ ...buttonData, class: 'btn--block', ...args })
