3efb3f2f32ef682f84c0790317894f7aafc7fa70
Hillel Arnold <helrond@hotmail.com>
2021-04-28 18:38:47 -0400

    mdx proof of concept

--- stories/badge/Badge.stories.js
+++ stories/badge/Badge.stories.js
@@ -1,17 +1,22 @@
 import '../../dist/main.min.css'
 
 import Badge from './badge.handlebars'
-import colors from './colors.json'
+import badgeColors from './colors.json'
 
-export default {
-  title: 'Components/Badges',
-  component: Badge,
-  args: {
-    text: '12 matches'
-  }
+// export default {
+//   title: 'Components/Badges',
+//   component: Badge,
+//   args: {
+//     text: '12 matches'
+//   }
+// }
+
+export const colors = (args) => {
+  const badges = badgeColors.map(c => Badge({ class: c, ...args })).join('')
+  return badges
 }
 
-export const Colors = (args) => {
-  const badges = colors.map(c => Badge({ class: c, ...args })).join('')
+export const basic = (args) => {
+  const badges = badgeColors.map(c => Badge({ class: c, ...args })).join('')
   return badges
 }
