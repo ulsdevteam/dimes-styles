c5f13442bfc6fac71534f88dcc306940d366bb6b
Hillel Arnold <helrond@hotmail.com>
2021-04-28 18:41:23 -0400

    cleaning up

--- stories/badge/Badge.stories.js
+++ stories/badge/Badge.stories.js
@@ -3,20 +3,7 @@ import '../../dist/main.min.css'
 import Badge from './badge.handlebars'
 import badgeColors from './colors.json'
 
-// export default {
-//   title: 'Components/Badges',
-//   component: Badge,
-//   args: {
-//     text: '12 matches'
-//   }
-// }
-
 export const colors = (args) => {
-  const badges = badgeColors.map(c => Badge({ class: c, ...args })).join('')
-  return badges
-}
-
-export const basic = (args) => {
   const badges = badgeColors.map(c => Badge({ class: c, ...args })).join('')
   return badges
 }
