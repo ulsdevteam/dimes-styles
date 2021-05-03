fcd2beedafec6977f734641fa45d2b136f0d94aa
Hillel Arnold <helrond@hotmail.com>
2021-05-03 13:14:58 -0400

    convert table of contents to mdx

--- stories/tableOfContents/TableOfContents.stories.js
+++ stories/tableOfContents/TableOfContents.stories.js
@@ -1,25 +1,9 @@
 import TableOfContents from './tableOfContents.handlebars'
 
-export default {
-  title: 'Components/Table of Contents',
-  component: TableOfContents,
-  args: {
-    ariaLabel: 'table of contents',
-    listItems: [
-      { text: 'Purpose', active: true },
-      { text: 'Objectives', active: false },
-      { text: 'Mandate', active: false },
-      { text: 'Scope', active: false },
-      { text: 'Challenges', active: false },
-      { text: 'Principles', active: false }
-    ]
-  }
-}
-
-export const Colors = (args) => {
-  const colors = ['toc--neutral', 'toc--blue', 'toc--orange']
-  const tocs = colors.map(c => TableOfContents({ class: c, ...args })).join('')
+export const colors = (args) => {
+  const componentColors = ['toc--neutral', 'toc--blue', 'toc--orange']
+  const tocs = componentColors.map(c => TableOfContents({ class: c, ...args })).join('')
   return tocs
 }
 
-export const WithTitle = (args) => TableOfContents({ class: 'toc--neutral', title: 'Digital Preservation Policy', ...args })
+export const withTitle = (args) => TableOfContents({ class: 'toc--neutral', title: 'Digital Preservation Policy', ...args })
