05ecb3c9abcd9b42fda343744ef21b3eb645fc6c
Hillel Arnold <helrond@hotmail.com>
2021-05-03 13:13:42 -0400

    convert search to mdx

--- stories/search/Search.stories.js
+++ stories/search/Search.stories.js
@@ -1,14 +1,36 @@
 import Search from './search.handlebars'
 
-export default {
-  title: 'Components/Search',
-  component: Search,
-  args: {
-    hideLabel: true,
-    controlsBlock: true,
-    controlsInline: false,
-    button: true
-  }
-}
+export const basic = () => Search({
+  hideLabel: false,
+  controlsBlock: false,
+  controlsInline: false,
+  button: true
+})
 
-export const Default = (args) => Search(args)
+export const noButton = () => Search({
+  hideLabel: false,
+  controlsBlock: false,
+  controlsInline: false,
+  button: false
+})
+
+export const noLabel = () => Search({
+  hideLabel: true,
+  controlsBlock: false,
+  controlsInline: false,
+  button: true
+})
+
+export const withBlockControls = () => Search({
+  hideLabel: false,
+  controlsBlock: true,
+  controlsInline: false,
+  button: true
+})
+
+export const withInlineControls = () => Search({
+  hideLabel: false,
+  controlsBlock: false,
+  controlsInline: true,
+  button: true
+})
