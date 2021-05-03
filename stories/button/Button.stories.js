73d7689f7593671694c6a2e42d8c4cc057121107
Hillel Arnold <helrond@hotmail.com>
2021-05-03 13:08:01 -0400

    convert button to mdx

--- stories/button/Button.stories.js
+++ stories/button/Button.stories.js
@@ -1,31 +1,24 @@
-import '../../dist/main.min.css'
-
 import Button from './button.handlebars'
-import colors from './colors.json'
-import sizes from './sizes.json'
+import Link from './link.handlebars'
+import buttonColors from './colors.json'
+import buttonSizes from './sizes.json'
 
-export default {
-  title: 'Components/Buttons',
-  component: Button,
-  args: {
-    text: 'Click me'
-  }
-}
-
-export const Colors = (args) => {
-  const buttons = colors.map(c => Button({ class: c, ...args })).join('')
+export const colors = (args) => {
+  const buttons = buttonColors.map(c => Button({ class: c, ...args })).join('')
   return buttons
 }
 
-export const Sizes = (args) => {
-  const buttons = sizes.map(s => Button({ class: s, ...args })).join('')
+export const sizes = (args) => {
+  const buttons = buttonSizes.map(s => Button({ class: s, ...args })).join('')
   return buttons
 }
 
-export const Icons = (args) => Button({ iconAfter: 'email', class: 'btn-primary', ...args })
-Icons.args = {
+export const icons = (args) => Button({ iconAfter: 'email', class: 'btn-primary', ...args })
+icons.args = {
   iconBefore: 'email',
   iconAfter: ''
 }
 
-export const FullWidth = (args) => Button({ class: 'btn--block', ...args })
+export const fullWidth = (args) => Button({ class: 'btn--block', ...args })
+
+export const link = (args) => Link(args)
