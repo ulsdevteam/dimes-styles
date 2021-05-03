5ac7f66d9221f9562493c24a2443510b4e6dcf74
Hillel Arnold <helrond@hotmail.com>
2021-05-03 13:12:55 -0400

    convert hero to mdx

--- stories/hero/Hero.stories.js
+++ stories/hero/Hero.stories.js
@@ -1,14 +1,3 @@
 import Hero from './hero.handlebars'
-import Logo from '../assets/img/home_page_logo.svg'
 
-export default {
-  title: 'Components/Hero',
-  component: Hero,
-  args: {
-    altText: 'Rockefeller Archive Center',
-    logoUrl: Logo,
-    text: 'The Rockefeller Archive Center is a major repository and research center for the study of philanthropy and its impact throughout the world.'
-  }
-}
-
-export const Default = (args) => Hero(args)
+export const basic = (args) => Hero(args)
