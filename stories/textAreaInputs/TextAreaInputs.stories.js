bfdb0dd44163ff283a94eb193ee7576e10137c47
Hannah <hsistrunk@gmail.com>
2021-05-04 10:54:35 -0400

    remove duplicate export

--- stories/textAreaInputs/TextAreaInputs.stories.js
+++ stories/textAreaInputs/TextAreaInputs.stories.js
@@ -1,14 +1,5 @@
 import TextareaInput from './textareaInput.handlebars'
 
-export default {
-  title: 'Components/Textarea Inputs',
-  component: TextareaInput,
-  args: {
-    label: 'Message',
-    name: 'message'
-  }
-}
-
 export const basic = (args) => TextareaInput(args)
 
 export const required = (args) => TextareaInput({ ...args, required: true })
