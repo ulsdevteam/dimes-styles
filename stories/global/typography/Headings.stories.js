6d36c3bec11c5fd14c80a30eda0ecf008ba99c84
Hillel Arnold <helrond@hotmail.com>
2021-05-24 14:27:55 -0400

    separate out bottom border styles

--- stories/global/typography/Headings.stories.js
+++ stories/global/typography/Headings.stories.js
@@ -1,3 +1,6 @@
 import Headings from './headings.handlebars'
+import BorderedHeadings from './borderedHeadings.handlebars'
 
 export const headings = () => Headings()
+
+export const bordered = () => BorderedHeadings()
