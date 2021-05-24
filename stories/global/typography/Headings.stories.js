be9aa400140daaad3fa3592d0cbde240d1e43ef0
Hillel Arnold <helrond@hotmail.com>
2021-05-24 14:46:57 -0400

    make heading docs consistent with links

--- stories/global/typography/Headings.stories.js
+++ stories/global/typography/Headings.stories.js
@@ -1,6 +1,3 @@
 import Headings from './headings.handlebars'
-import BorderedHeadings from './borderedHeadings.handlebars'
 
 export const headings = () => Headings()
-
-export const bordered = () => BorderedHeadings()
