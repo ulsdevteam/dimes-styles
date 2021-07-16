537403ac27132f0a22c1b18efb40440566073563
Hillel Arnold <helrond@hotmail.com>
2021-07-16 13:22:46 -0400

    update text input stories

--- stories/textInputs/TextInputs.stories.js
+++ stories/textInputs/TextInputs.stories.js
@@ -4,4 +4,6 @@ export const basic = (args) => TextInput(args)
 
 export const required = (args) => TextInput({ ...args, required: true })
 
+export const hideLabel = (args) => TextInput({ ...args, hideLabel: true })
+
 export const withError = (args) => TextInput({ ...args, required: true, invalid: true })
