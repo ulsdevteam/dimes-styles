c38b9306fecacf3d3a75acf9d4fda6dbcf9374a4
Hannah <hsistrunk@gmail.com>
2021-09-15 09:31:49 -0400

    doc when to use color options and show in stories

--- stories/header/Header.stories.js
+++ stories/header/Header.stories.js
@@ -1,46 +1,51 @@
 import Header from './header.handlebars'
 import headerColors from './colors.json'
 import Logo from '../assets/img/rac_logo.svg'
+import { BUILDER_KEYS } from '@babel/types'
 
 export const colors = () => {
   const headers = headerColors.map(c => Header({
     class: c,
-    withBrandText: true,
+  })).join('')
+  return headers
+}
+
+export const withTextBrand = () => {
+  const headers = headerColors.map(c => Header({
+    class: c,
+    withTextBrand: true,
     brandTitle: 'library.rockarch.org',
     brandSubtitle: 'The Online Bibliographic Catalog of Rockefeller Archive Center'
   })).join('')
   return headers
 }
 
-export const withTextBrand = () => Header(
-  {
-    withTextBrand: true,
-    brandTitle: 'library.rockarch.org',
-    brandSubtitle: 'The Online Bibliographic Catalog of Rockefeller Archive Center'
-  }
-)
-
-export const withImageBrand = () => Header(
-  {
+export const withImageBrand = () => {
+  const headers = headerColors.map(c => Header({
+    class: c,
     withImageBrand: true,
     logoUrl: Logo
-  }
-)
+  })).join('')
+  return headers
+}
 
 export const withNavItems = () => Header(
   {
+    class: "header--blue",
     withNavItems: true
   }
 )
 
 export const withDropdownItems = () => Header(
   {
+    class: "header--white",
     withDropdownItems: true
   }
 )
 
 export const withSocialIcons = () => Header(
   {
+    class: "header--white",
     withSocialIcons: true
   }
 )
