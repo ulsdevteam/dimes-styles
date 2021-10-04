1d8d37c810296b7dce223e9e658f0eca9c0c7577
Hillel Arnold <helrond@hotmail.com>
2021-10-04 11:51:03 -0400

    add additional minimap styling

--- stories/minimap/Minimap.stories.js
+++ stories/minimap/Minimap.stories.js
@@ -1,3 +1,7 @@
 import Minimap from './minimap.handlebars'
 
 export const basic = () => Minimap()
+
+export const tenAcross = () => Minimap({ count: 10 })
+
+export const twentyAcross = () => Minimap({ count: 20 })
