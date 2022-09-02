6cdfcbd5015d456447317f7083233191eb035491
Hannah <hsistrunk@gmail.com>
2022-09-02 09:22:38 -0400

    fix build script

--- script/build_release.sh
+++ script/build_release.sh
@@ -4,5 +4,6 @@ if ! [ -z "$1" ]
   then
     mkdir dist/$1
     cp dist/main.min.css dist/$1/main.min.css
-    cp assets/ dist/$1/
+    cp -r stories/assets/ dist/
+    cp -r stories/assets/ dist/$1/
 fi
