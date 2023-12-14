2d5513ec0d70eaaa0256e9f98eeb10dab4097cfb
Hillel Arnold <hillel.arnold@gmail.com>
2023-12-14 13:33:15 -0500

    separate inputs to additional story

--- stories/components/table/Table.stories.js
+++ stories/components/table/Table.stories.js
@@ -2,10 +2,15 @@ import Table from './table.handlebars'
 
 export default {
   component: Table,
-  title: "Components/Table",
-  args:{
-    striped: true,
-  }
+  title: "Components/Table"
 };
 
-export const basic = (args) => Table(args)
+export const basic = () => Table({
+  inputs: false,
+  striped: true
+})
+
+export const withInputs = () => Table({
+  striped: true,
+  inputs: true
+})
