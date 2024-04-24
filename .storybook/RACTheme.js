2e27e66c74278cb1adbf9807fbcda976fbef2dd0
trl75 <trl75@pitt.edu>
2024-04-24 17:07:51 -0400

    Updated storybook's theme from RAC to Pitt but not renamed

--- .storybook/RACTheme.js
+++ .storybook/RACTheme.js
@@ -1,8 +1,8 @@
 import { create } from '@storybook/theming'
-import Logo from '../stylesheets/assets/img/rac_logo_small.svg'
+import Logo from '../stylesheets/assets/img/logo.svg'
 
 export default create({
   base: 'light',
-  brandTitle: 'Rockefeller Archive Center Style Guide',
+  brandTitle: 'Pitt Branding applying over RACs',
   brandImage: Logo
 })
